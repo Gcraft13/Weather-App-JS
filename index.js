2c3554a11f3452c7d756dd93695c56029a78f969
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-10-18 11:41:23 -0400

    add jquery library

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const apiKey = "727e0ab48aaefb4ca2fdb3d0ffd616d4";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+
+$(".btn").click(function () {
+  console.log("I've been clicked!");
+});
