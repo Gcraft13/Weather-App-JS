ce7c3f7d70589b7c5a7b3fc3ed9127bef10001cf
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-10-18 16:01:58 -0400

    add code for getData and fetch function for API GET request

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const apiKey = "727e0ab48aaefb4ca2fdb3d0ffd616d4";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+const apiKey = "&appid=727e0ab48aaefb4ca2fdb3d0ffd616d4";
+
+async function getData() {
+  const apiURL =
+    "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=Virginia";
+  try {
+    const response = await fetch(`${apiURL}  +${apiKey}`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 $(".btn").click(function () {
-  console.log("I've been clicked!");
+  getData();
 });
