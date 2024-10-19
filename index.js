2ea4db4a460ef8b9982eaa7e123e020c50fd2f71
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-10-19 13:06:27 -0400

    add switch statement to change weather icon

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-const apiKey = "&appid=727e0ab48aaefb4ca2fdb3d0ffd616d4";
+const apiKey = "727e0ab48aaefb4ca2fdb3d0ffd616d4";
+const city = $("input");
 
-async function getData() {
+async function getData(city) {
   const apiURL =
-    "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=Virginia";
+    "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+
   try {
-    const response = await fetch(`${apiURL}  +${apiKey}`);
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const json = await response.json();
-    console.log(json);
+    const data = await response.json();
+    console.log(data);
+
+    $(".city-name").text(data.name);
+    $(".temperature").text(Math.round(data.main.temp) + "°F");
+    $(".percent").text(data.main.humidity + "%");
+    $(".speed").text(Math.round(data.wind.speed) + " mph");
+
+    let weather = data.weather[0].main;
+
+    switch (weather) {
+      case "Clouds":
+        $("#weather-icon").attr("src", "./images/clouds.png");
+        break;
+      case "Clear":
+        $("#weather-icon").attr("src", "./images/clear.png");
+        break;
+      case "Drizzle":
+        $("#weather-icon").attr("src", "./images/drizzle.png");
+        break;
+      case "Mist":
+        $("#weather-icon").attr("src", "./images/mist.png");
+        break;
+      case "Rain":
+        $("#weather-icon").attr("src", "./images/rain.png");
+        break;
+      case "Snow":
+        $("#weather-icon").attr("src", "./images/snow.png");
+        break;
+
+      // code block
+    }
   } catch (error) {
     console.error(error.message);
+    alert("Please try another city");
   }
 }
 
 $(".btn").click(function () {
-  getData();
+  getData(city.val());
 });
